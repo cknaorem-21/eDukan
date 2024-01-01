ea2671176ad27f702e064bb3f2c95328eb5fbca7
CK <cknaorem.mnnit@gmail.com>
2024-01-01 22:46:30 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config()
 
+import connectDB from './config/db.js';
+
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
