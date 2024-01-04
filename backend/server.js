e5392ec7c9a171bde78b607d5310ee6a555315e3
CK <cknaorem.mnnit@gmail.com>
2024-01-04 13:56:38 +0530

    backend connected with database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js'
 import dotenv from 'dotenv';
-dotenv.config()
-
 import connectDB from './config/db.js';
+
+dotenv.config()
 
 const port = process.env.PORT || 5000;
 
@@ -15,14 +15,7 @@ app.get('/', (req, res) => {
     res.send('get request response')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((product) => product._id === req.params.id)
-    res.json(product);
-})
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
